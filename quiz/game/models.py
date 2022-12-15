3eddb258fea91bff1376e518fa109655d12d62a1
badsnus <badsnus@mail.ru>
2022-12-15 21:11:10 +0500

    verbose names for models and register them in admin

--- quiz/game/models.py
+++ quiz/game/models.py
@@ -4,9 +4,17 @@ from django.contrib.auth.models import User
 
 from . import managers
 
+
 class GameQuestion(models.Model):
-    question = models.CharField(max_length=300)
-    answer = models.CharField(max_length=300)
+    question = models.CharField(max_length=300, verbose_name='вопрос')
+    answer = models.CharField(max_length=300, verbose_name='ответ')
+
+    def __str__(self):
+        return f'вопрос {self.pk}'
+
+    class Meta:
+        verbose_name = 'вопрос'
+        verbose_name_plural = 'вопросы'
 
 
 class GameMember(models.Model):
@@ -14,39 +22,81 @@ class GameMember(models.Model):
 
     name = models.CharField(max_length=30, verbose_name='имя')
 
-    game = models.ForeignKey('Game', on_delete=models.CASCADE)
+    game = models.ForeignKey(
+        'Game',
+        on_delete=models.CASCADE,
+        verbose_name='игра'
+    )
 
-    good_answers = models.IntegerField(default=0)
-    bad_answers = models.IntegerField(default=0)
-    brought_in_bank = models.IntegerField(default=0)
+    good_answers = models.IntegerField(
+        default=0,
+        verbose_name='правильных ответов'
+    )
+    bad_answers = models.IntegerField(
+        default=0,
+        verbose_name='неправильных ответов'
+    )
+    brought_in_bank = models.IntegerField(
+        default=0,
+        verbose_name='принес в банк'
+    )
 
-    out_of_game = models.BooleanField(default=False)
+    out_of_game = models.BooleanField(default=False, verbose_name='вылетел')
+
+    def __str__(self):
+        return f'участник {self.pk}'
+
+    class Meta:
+        verbose_name = 'участник'
+        verbose_name_plural = 'участники'
 
 
 class GameRound(models.Model):
-    game = models.ForeignKey('Game', on_delete=models.CASCADE)
+    game = models.ForeignKey(
+        'Game',
+        on_delete=models.CASCADE,
+        verbose_name='игра'
+    )
 
-    bank = models.IntegerField(default=0)
-    now_bank = models.IntegerField(default=0)
-    end_time = models.DateTimeField(null=True)
-    vote = models.BooleanField(default=False)
-    final = models.BooleanField(default=False)
-    ended = models.BooleanField(default=False)
-    offset = models.IntegerField(default=0)
+    bank = models.IntegerField(default=0, verbose_name='банк')
+    now_bank = models.IntegerField(default=0, verbose_name='текущий банк')
+    end_time = models.DateTimeField(null=True, verbose_name='время окончания')
+    vote = models.BooleanField(default=False, verbose_name='этап голосования')
+    final = models.BooleanField(default=False, verbose_name='финальный раунд')
+    ended = models.BooleanField(default=False, verbose_name='раунд закончился')
+    offset = models.IntegerField(default=0, verbose_name='номер отвечающего')
+
+    def __str__(self):
+        return f'раунд {self.pk}'
+
+    class Meta:
+        verbose_name = 'раунд'
+        verbose_name_plural = 'раунды'
 
 
 class Game(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='владелец игры'
+    )
 
-    bank = models.IntegerField(default=0)
+    bank = models.IntegerField(default=0, verbose_name='банк')
     start_round_time = models.IntegerField(
         default=150,
         verbose_name='начальное время на раунд',
         validators=[
             MaxValueValidator(1800),
             MinValueValidator(120)
-        ]
+        ],
     )
 
-    started = models.BooleanField(default=False)
-    ended = models.BooleanField(default=False)
+    started = models.BooleanField(default=False, verbose_name='игра начата')
+    ended = models.BooleanField(default=False, verbose_name='игра закончена')
+
+    def __str__(self):
+        return f'игра {self.pk}'
+
+    class Meta:
+        verbose_name = 'игра'
+        verbose_name_plural = 'игры'
