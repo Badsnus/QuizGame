353411887147b007aad45b69150bc15957768dc4
badsnus <badsnus@mail.ru>
2022-12-18 10:21:35 +0500

    update homepage tests

--- quiz/homepage/tests.py
+++ quiz/homepage/tests.py
@@ -1,15 +1,18 @@
-from django.test import Client, TestCase
+from django.test import TestCase
+from django.urls import reverse
 
 
 class HomepageTestCase(TestCase):
+    HOMEPAGE_URL = reverse('home:homepage')
+
     def test_homepage_get(self):
-        response = Client().get('/')
+        response = self.client.get(self.HOMEPAGE_URL)
         self.assertEqual(response.status_code, 200)
 
     def test_homepage_post(self):
-        response = Client().post('/')
+        response = self.client.post(self.HOMEPAGE_URL)
         self.assertEqual(response.status_code, 405)
 
     def test_homepage_with_param(self):
-        response = Client().get('/1/')
+        response = self.client.get(self.HOMEPAGE_URL + '1/')
         self.assertEqual(response.status_code, 404)
