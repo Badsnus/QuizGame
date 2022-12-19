63cb0c14b608c814752db7ca1573643ae8c44227
Karonus <grant.makyan@gmail.com>
2022-12-19 22:29:42 +0300

    minor changes

--- quiz/settings/settings.py
+++ quiz/settings/settings.py
@@ -71,9 +71,9 @@ WSGI_APPLICATION = 'settings.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB', 'quizgame'),
-        'USER': os.environ.get('POSTGRES_USER', 'quizgame'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'supertopandsecretpassword'),
+        'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
         'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
         'PORT': '5432',
     }
