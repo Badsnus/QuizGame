3c85aec078511768146b368130d7d63acaaf6b33
Karonus <grant.makyan@gmail.com>
2022-12-19 22:04:08 +0300

    Updated LogoutRequiredMixin mixin

--- quiz/users/mixins.py
+++ quiz/users/mixins.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.mixins import AccessMixin
+from django.shortcuts import redirect
 
 
 class LogoutRequiredMixin(AccessMixin):
     """Verify that the current user is not authenticated."""
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
-            return self.handle_no_permission()
+            return redirect("users:profile")
 
         return super().dispatch(request, *args, **kwargs)
